b4353bdf8de207b44387cec835e1aa5c018f9d7c
Nathan Alegria <nathanalegria02@gmail.com>
2025-11-14 08:39:35 -0600

    Interface MenuActions

---
+++ Examen/src/examen/Movie.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Nathan
+ */
+public class Movie extends RentItem {
+    
+}
